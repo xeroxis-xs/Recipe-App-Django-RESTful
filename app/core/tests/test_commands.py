5a2dcaaefb57a521443da18e35d7af50a9a8615f
xeroxis-xs <txsheng0609@hotmail.com>
2023-05-09 10:34:14 +0800

    Setup Django Admin Part 1

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,10 +1,9 @@
 """
 Test custom Django management commands.
 """
-
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -13,7 +12,7 @@ from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    """Test commnads."""
+    """Test commands."""
 
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
@@ -26,7 +25,7 @@ class CommandTests(SimpleTestCase):
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
-        patched_check.side_effect = [Psycopg2Error] * 2 + \
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
