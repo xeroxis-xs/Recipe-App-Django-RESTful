0f11447fa250352df4c53789671334b73217a97c
xeroxis-xs <txsheng0609@hotmail.com>
2023-05-16 17:21:41 +0800

    Set up Admin and User

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ Django admin customisation.
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as translate
 
 from core import models
 
@@ -11,6 +12,42 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            translate('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (
+            translate('Important dates'),
+            {
+                'fields': (
+                    'last_login',
+                )
+            }
+        ),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
